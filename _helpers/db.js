4c72b74792cef4257459b5a7213e6d2471870405
MohammadAshfan12 <46986393+MohammadAshfan12@users.noreply.github.com>
2020-05-26 12:53:24 -0700

    Delete db.js

--- _helpers/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect(process.env.URL,{ useCreateIndex: true, useNewUrlParser: true ,useUnifiedTopology: true })
-
-module.exports={
-    Category:require('../models/category'),
-    Product:require('../models/product')
-}
